064cdb075199ac5f31deb2a7bb4d2e2026d24942
Dr.Pierrot <Capulongako16@gmail.com>
2023-02-01 19:49:01 +0800

    edited the cart.php

--- Cart.php
+++ Cart.php
@@ -2,3 +2,18 @@
     require_once "Medicine.php";
     class Cart(){
         private $cartItems = [];
+
+        function addToCart($cartItems){
+            $this->cartItems = $cartItems;
+        }
+        function viewCart(){
+            foreach ($this->cartItems as $value){
+                echo $value."<br>";
+            }
+        }
+        function computeTotal($srp){
+            echo "<hr><br>";
+            echo $this->srp;
+        }
+    }
+?>
