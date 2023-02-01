13daea2096374a46ab2505cdd006e6289977adc5
Dr.Pierrot <Capulongako16@gmail.com>
2023-02-01 19:14:39 +0800

    Medicine.php successfully created!

---
+++ Medicine.php
@@ -0,0 +1,29 @@
+<?php
+    require_once "Products.php";
+
+    class Medicine extends Products {
+        private $dose;
+        private $type;
+        private $exoirationDate;
+
+        function setDose($){
+
+        }
+        function setType(){
+            
+        }
+        function setExpirationDate(){
+            
+        }
+        function getDose(){
+
+        }
+        function getType(){
+            
+        }
+        function getExpirationDate(){
+            
+        }
+    }
+
+?>
